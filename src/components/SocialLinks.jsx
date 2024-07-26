f7954322775322ba36b129b45e4c0879e408b7ba
KiwiScreams <screamskiwi3@gmail.com>
2024-07-27 01:36:59 +0400

    social page

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -7,119 +7,224 @@ import facebook_icon from "../assets/images/facebook.svg"
 import twitter_icon from "../assets/images/twitter.svg"
 import camera from "../assets/images/Camera Icon.svg"
 function SocialLinks() {
+    const [todo, setTodo] = useState([]);
     const [input, setInput] = useState('');
-    const [todo, setTodo] = useState([
-        { id: Math.random(), name: "YouTube", href: "https://www.facebook.com/NanukaZhorzholianiShow", image: youtube_icon },
-        { id: Math.random(), name: "FaceBook", href: "https://www.facebook.com/NanukaZhorzholianiShow", image: facebook_icon },
-        { id: Math.random(), name: "Twitter", href: "https://www.facebook.com/NanukaZhorzholianiShow", image: twitter_icon },
-        { id: Math.random(), name: "YouTube", href: "https://www.facebook.com/NanukaZhorzholianiShow", image: youtube_icon },
-        { id: Math.random(), name: "FaceBook", href: "https://www.facebook.com/NanukaZhorzholianiShow", image: facebook_icon },
-        { id: Math.random(), name: "Twitter", href: "https://www.facebook.com/NanukaZhorzholianiShow", image: twitter_icon },
-        { id: Math.random(), name: "YouTube", href: "https://www.facebook.com/NanukaZhorzholianiShow", image: youtube_icon },
-        { id: Math.random(), name: "FaceBook", href: "https://www.facebook.com/NanukaZhorzholianiShow", image: facebook_icon },
-        { id: Math.random(), name: "Twitter", href: "https://www.facebook.com/NanukaZhorzholianiShow", image: twitter_icon }
-    ]);
-    const [recentImageURL, setRecentImageURL] = useState(null);
-    // useEffect(() => {
-    //     const storedImage = localStorage.getItem("social-image");
-    //     if (storedImage) {
-    //       setRecentImageURL(storedImage);
-    //     }
-    //   }, []);
-      const handleImageChange = (event, todoId) => {
-        const file = event.target.files[0]
-        const reader = new FileReader()
-        reader.addEventListener("load", () => {
-          const imageDataURL = reader.result;
-          setTodo(todo.map((item) =>
-            item.id === todoId ? { ...item, recentImageURL: imageDataURL } : item
-          ))
-        //   localStorage.setItem(`social-image`, imageDataURL)
-        })
-        reader.readAsDataURL(file)
-      }
-      
     const [updateToDo, setUpdateToDo] = useState(null);
-    const [IsToggle, setToggle] = useState(true);
-    const AddItem = (e) => {
-        e.preventDefault();
-        if (!input) {
-            alert("Please Fill Input Value");
-        } else if (input === " ") {
-            alert("Please Fill Input Value");
-        }
-        else if (updateToDo) {
-            setTodo(todo.map((e) => {
+    const [toggle, setToggle] = useState(true);
+    const [image, setImage] = useState(null);
+    const [name, setName] = useState('');
+    const [href, setHref] = useState('');
+    const [imageUrl, setImageUrl] = useState(null);
+    const [showPanel, setShowPanel] = useState(false);
+    const handleImageChange = (event) => {
+        setImage(event.target.files[0]);
+        const imageUrl = URL.createObjectURL(event.target.files[0]);
+        setImageUrl(imageUrl);
+    };
+
+    const AddItem = () => {
+        if (updateToDo) {
+            const updatedTodo = todo.map((e) => {
                 if (e.id === updateToDo) {
-                    return { ...e, name: input }
+                    return { ...e, name: name, href: href, image: image };
                 }
                 return e;
-            }))
-            setInput('');
-            setToggle(true);
+            });
+            setTodo(updatedTodo);
             setUpdateToDo(null);
+            setToggle(true);
+            setImage(null);
+            setName('');
+            setHref('');
         } else {
-            const AllInput = { id: Math.random(), name: input }
-            setTodo([...todo, AllInput]);
-            setInput('')
+            const newTodo = {
+                id: Math.random(),
+                name: name,
+                href: href,
+                image: image,
+            };
+            setTodo([...todo, newTodo]);
+            setImage(null);
+            setName('');
+            setHref('');
         }
-    }
+    };
+
     const DeleteItem = (ToDo) => {
-        const TrashData = todo.filter((e) => {
-            return e.id !== ToDo.id
-        })
-        setTodo(TrashData);
-    }
+        const filteredTodo = todo.filter((e) => e.id !== ToDo.id);
+        setTodo(filteredTodo);
+    };
+
     const EditItem = (ToDo) => {
-        const UpdateFind = todo.find((e) => {
-            return e.id === ToDo.id
-        })
-        setInput(UpdateFind.name);
-        setUpdateData(ToDo.id);
+        const updatedTodo = todo.find((e) => e.id === ToDo.id);
+        setName(updatedTodo.name);
+        setHref(updatedTodo.href);
+        setImage(updatedTodo.image);
+        setUpdateToDo(ToDo.id);
         setToggle(false);
-    }
+    };
+
+    const handleAddSocClick = () => {
+        setShowPanel(true);
+    };
+
+    const handleClosePanel = () => {
+        setShowPanel(false);
+    };
+
     return (
-        <>
-            <div className="social-links-container">
-                <div className="social-links-header">
-                    <h5 className="text-bg">სოციალური ბმულები</h5>
-                </div>
-                <div className="social-links-body">
-                    <input type="text" placeholder='Today Plan' onChange={(e) => {
-                        setInput(e.target.value);
-                    }} value={input}></input>
-                    <button onClick={AddItem}> ADD </button>
-                    <div className='all-todos-container'>
-                        {todo?.map((e) => {
-                            return (
-                                <div key={e.id} className="flex todo">
-                                    <div className="flex todo-content">
-                                        <div className="soc-image">
-                                        <img src={e.recentImageURL ? e.recentImageURL : e.image} className="soc"/>
-                                            <div className="choose-image">
-                                                <input type="file" id="image-upload" onChange={(event) => handleImageChange(event, e.id)}/>
-                                                <img src={camera} alt="" id="avatar" />
-                                            </div>
-                                        </div>
-                                        <input type="text" value={e.name} disabled className="todo-title" />
-                                        <input type="text" value={e.href} disabled className="todo-href"/>
-                                    </div>
-                                    <div className="flex edit-delete-btn-container">
-                                        <div>
-                                            <button onClick={() => EditItem(e)}><img src={editBtn} /></button>
-                                        </div>
-                                        <div>
-                                            <button onClick={() => DeleteItem(e)}><img src={deleteBtn} /></button>
-                                        </div>
+        // <div className="social-links-container">
+        //     <div className="social-links-header">
+        //         <h5 className="text-bg">სოციალური ბმულები</h5>
+        //     </div>
+        //     <div className="social-links-body">
+        //         <input
+        //             type="file"
+        //             onChange={handleImageChange}
+        //             placeholder="აირჩიე სოციალური ქსელის ხატულა"
+        //         />
+        //         <input
+        //             type="text"
+        //             value={name}
+        //             onChange={(e) => setName(e.target.value)}
+        //             placeholder="Name of social network"
+        //         />
+        //         <input
+        //             type="text"
+        //             value={href}
+        //             onChange={(e) => setHref(e.target.value)}
+        //             placeholder="Link"
+        //         />
+        //         <button onClick={AddItem}>ADD</button>
+        //         <div className="all-todos-container">
+        //             {todo.map((e) => (
+        //                 <div key={e.id} className="flex todo">
+        //                     <div className="flex todo-content">
+        //                         <div className="soc-image">
+        //                             <img src={e.image} className="soc" />
+        //                             <div className="choose-image">
+        //                                 <input
+        //                                     type="file"
+        //                                     id="image-upload"
+        //                                     onChange={(event) => handleImageChange(event, e.id)}
+        //                                 />
+        //                                 {imageUrl && (
+        //                                     <img src={imageUrl} alt="Chosen Image" style={{ width: 100, height: 100 }} className="soc-image" />
+        //                                 )}
+        //                                 <img src={camera} alt="" id="avatar" />
+        //                             </div>
+        //                         </div>
+        //                         <input type="text" value={e.name} disabled className="todo-title" />
+        //                         <input type="text" value={e.href} disabled className="todo-href" />
+        //                     </div>
+        //                     <div className="flex edit-delete-btn-container">
+        //                         <div>
+        //                             <button onClick={() => EditItem(e)}>
+        //                                 <img src={editBtn} />
+        //                             </button>
+        //                         </div>
+        //                         <div>
+        //                             <button onClick={() => DeleteItem(e)}>
+        //                                 <img src={deleteBtn} />
+        //                             </button>
+        //                         </div>
+        //                     </div>
+        //                 </div>
+        //             ))}
+        //         </div>
+        //         <button className="goback-btn text-bg add-soc" onClick={handleAddSocClick}>
+        //             დაამატე ახალი სოციალური ბმული
+        //         </button>
+        //         <div className="all-todos-container">
+        //             {todo.map((e) => (
+        //                 <div key={e.id} className="flex todo">
+        //                     <div className="flex todo-content">
+        //                         <div className="soc-image">
+        //                             <img src={e.image} className="soc" />
+        //                             <div className="choose-image">
+        //                                 <input
+        //                                     type="file"
+        //                                     onChange={handleImageChange}
+        //                                     placeholder="აირჩიე სოციალური ქსელის ხატულა"
+        //                                 />
+        //                             </div>
+        //                         </div>
+        //                     </div>
+        //                 </div>
+        //             ))}
+        //         </div>
+        //     </div>
+        // </div>
+        <div className="social-links-container">
+            <div className="social-links-header">
+                <h5 className="text-bg">სოციალური ბმულები</h5>
+            </div>
+            <div className="social-links-body">
+                {showPanel && (
+                    <div className="add-soc-panel">
+                        <div className="panel-header">
+                            <h5>Add Social Link</h5>
+                            <button onClick={handleClosePanel}>Close</button>
+                        </div>
+                        <input
+                            type="file"
+                            onChange={handleImageChange}
+                            placeholder="აირჩიე სოციალური ქსელის ხატულა"
+                        />
+                        <input
+                            type="text"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            placeholder="Name of social network"
+                        />
+                        <input
+                            type="text"
+                            value={href}
+                            onChange={(e) => setHref(e.target.value)}
+                            placeholder="Link"
+                        />
+                        <button onClick={AddItem}>ADD</button>
+                    </div>
+                )}
+                <button onClick={handleAddSocClick} className="goback-btn text-bg add-soc">დაამატე ახალი სოციალური ბმული</button>
+                <div className="all-todos-container">
+                    {todo.map((e) => (
+                        <div key={e.id} className="flex todo">
+                            <div className="flex todo-content">
+                                <div className="soc-image">
+                                    <img src={e.image} className="soc" />
+                                    <div className="choose-image">
+                                        <input
+                                            type="file"
+                                            id="image-upload"
+                                            onChange={(event) => handleImageChange(event, e.id)}
+                                        />
+                                        {imageUrl && (
+                                            <img src={imageUrl} alt="Chosen Image" style={{ width: 100, height: 100 }} className="soc-image" />
+                                        )}
+                                        <img src={camera} alt="" id="avatar" />
                                     </div>
                                 </div>
-                            )
-                        })}
-                    </div>
-                    <button className="goback-btn text-bg">დაამატე ახალი სოციალური ბმული</button>
+                                <input type="text" value={e.name} disabled className="todo-title" />
+                                <input type="text" value={e.href} disabled className="todo-href" />
+                            </div>
+                            <div className="flex edit-delete-btn-container">
+                                <div>
+                                    <button onClick={() => EditItem(e)}>
+                                        <img src={editBtn} />
+                                    </button>
+                                </div>
+                                <div>
+                                    <button onClick={() => DeleteItem(e)}>
+                                        <img src={deleteBtn} />
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
                 </div>
             </div>
-        </>
-    )
+        </div>
+    );
 }
 export default SocialLinks
